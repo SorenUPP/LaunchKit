f4b40ca129c34d60920a5f760e5a1c1638fff877
SorenUPP <beigiemad@gmail.com>
2026-06-27 15:37:27 +0200

    fix

--- server/server.js
+++ server/server.js
@@ -18,6 +18,7 @@ app.use(cors({
 }));
 app.use(express.json({ limit: "10kb" }));
 app.use(cookieParser());
+app.set("trust proxy", 1);
 
 app.get("/", (req, res) => {
     res.json({ message: "API running" });
